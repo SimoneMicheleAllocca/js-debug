c2d1cd91ea9b68ec43bbd05de7de1fd4f95a07a0
SimoneMicheleAllocca <s.allocca1998@gmail.com>
2024-04-16 16:58:53 +0200

    second bundle

--- js-debug/bundle-02/main.js
+++ js-debug/bundle-02/main.js
@@ -22,6 +22,8 @@ function checkAge() {
     }
 }
 checkAge();
+// 1. Questa funzione verifica se myAge è inferiore a 18. Se è true, message sarà dei troppo giovane + hai (l'età che ha) + anni. Se è false, message sarà hai più di 18 anni
+// 2. siccome message dovrà essere modificato, è più corretto usare let invece di const
 
 // ESERCIZIO 2
 function printColorsNumber() {
@@ -29,6 +31,8 @@ function printColorsNumber() {
     console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
 }
 printColorsNumber();
+// 1. dovrebbe stampare il numero di colori
+// 2. length è stato scritto in modo sbagliato
 
 
 // ESERCIZIO 3
@@ -39,6 +43,8 @@ function addNumbers() {
     console.log(`Il risultato finale è ${total}`);
 }
 addNumbers();
+// 1. chiede all'utente un numero e successivamento dovrebbe aggiungere 12 al numero chiesto
+// 2. siccome non è stato insterito parseInt prima del prompt, il numero sarà una stringa e quindi avverrà la concatenazione e non addizione
 
 
 // ESERCIZIO 4
@@ -59,6 +65,8 @@ function checkAccess() {
     }
 }
 checkAccess();
+// 1. chiede all'utente l'email, se l'email è presente nell'array allora consente l'accesso altrimenti lo nega
+// 2. false e true sono state scritte come stringhe e non come booleano
 
 
 // ESERCIZIO 5 (suggerimento: c'è un solo errore)
@@ -88,7 +96,9 @@ function checkAccessImproved() {
         }
     }
     checkAccessImproved();
-
+    
+1. chiede all'utente di inserire la sua email, e controlla se è presente nell'Array. Poi verifica se la sua lunghezza è > 5.
+2.
 
 
 
